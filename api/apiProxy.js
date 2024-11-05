579d0032913355d6a8e12dcc281486d400d38167
djfusco <davidjfusco@gmail.com>
2024-11-05 12:17:40 -0500

    added console log

--- api/apiProxy.js
+++ api/apiProxy.js
@@ -40,6 +40,7 @@ export default async function handler(req, res) {
         }
 
         const data = await response.json();
+        console.log(data);
         res.status(200).json(data);
     } catch (error) {
         console.error("Error:", error);
